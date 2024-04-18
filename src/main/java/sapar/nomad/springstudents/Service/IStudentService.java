3753d89722ee790cc1ddc4e45fef2730cf473a8b
Sapar Sayat <saparsayat01@gmail.com>
2024-04-18 22:42:33 +0500

    Add some new features

--- src/main/java/sapar/nomad/springstudents/Service/IStudentService.java
+++ src/main/java/sapar/nomad/springstudents/Service/IStudentService.java
@@ -7,10 +7,12 @@ import java.util.List;
 public interface IStudentService {
     List<Student> findAllStudent();
 
+    // todo: Change returning type to void
     Student saveStudent(Student student);
 
     Student findByEmail(String email);
 
+    // todo: Change returning type to void
     Student updateStudent(Student student);
 
     void deleteStudent(String email);
